57ecfe5f992feaaf6ba5cbabdc051a8c7dfd899c
Mednnn <mednyo@gmail.com>
2022-10-03 11:54:04 +0300

    last updates

--- go.mod
+++ go.mod
@@ -2,14 +2,13 @@ module filesend
 
 go 1.19
 
-require github.com/coalalib/coalago v0.0.0-20220324012807-d0367ab5ec2a
+require github.com/coalalib/coalago v0.0.0-20220926104606-4d2fe58cd92a
 
 require (
-	github.com/chzyer/readline v1.5.1 // indirect
-	github.com/google/pprof v0.0.0-20220829040838-70bd9ae97f40 // indirect
-	github.com/ianlancetaylor/demangle v0.0.0-20220517205856-0058ec4f073c // indirect
 	github.com/lucas-clemente/aes12 v0.0.0-20171027163421-cd47fb39b79f // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
-	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
 )
+
+replace github.com/coalalib/coalago => ../coalago
--- main.go
+++ main.go
@@ -12,38 +12,47 @@ import _ "net/http/pprof"
 
 func main() {
 	mode := os.Args[1]
+	//mode := "client"
 	f, _ := os.Create(fmt.Sprintf("pprof_%s", mode))
 
 	switch mode {
 	case "server":
-		//http.ListenAndServe("0.0.0.0:8000", nil)
 		srv := coalago.NewServer()
 		fcontent, err := os.ReadFile("testfile20mb")
 		if err != nil {
 			print(err)
 		}
+		println(len(fcontent))
+		println("9825")
 		pprof.StartCPUProfile(f)
 		srv.AddGETResource("/file", func(message *coalago.CoAPMessage) *coalago.CoAPResourceHandlerResult {
+			println("connect")
 			return coalago.NewResponse(coalago.NewBytesPayload(fcontent), coalago.CoapCodeContent)
 		})
-		go srv.Listen(":1111")
+		srv.Listen(":9825")
 		print("start wait")
-		time.Sleep(time.Second * 20)
+		time.Sleep(time.Second * 30)
 		print("stop")
 		pprof.StopCPUProfile()
 
 	case "client":
-
 		cl := coalago.NewClient()
 		var resp *coalago.Response
 		var err error
 		pprof.StartCPUProfile(f)
-		resp, err = cl.GET("coap://127.0.0.1:1111/file")
+		resp, err = cl.GET("coap://147.182.133.37:9825/file")
 		pprof.StopCPUProfile()
 		if err != nil {
-			print(err)
+			print(err.Error())
 		}
-		os.WriteFile("testfileDone", resp.Body, 7777)
+		if resp != nil {
+			os.WriteFile("testfileDone", resp.Body, 7777)
+		}
 	}
 
 }
+
+//9825
+//147.182.133.37
+//5.129.81.97
+//138.197.191.160
